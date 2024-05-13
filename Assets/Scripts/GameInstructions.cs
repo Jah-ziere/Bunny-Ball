dfe2b64d987d555b9df26bb7d5b6236073e428da
Jah-ziere <146846048+Jah-ziere@users.noreply.github.com>
2024-05-13 09:22:09 -0400

    Initial commit

---
+++ Assets/Scripts/GameInstructions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Use the W, a, s, d or arrow keys to move the player around");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
